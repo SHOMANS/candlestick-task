060a01bd3265039ebaa1f2933c85aae64954acc2
SHOMANS <mohammedshoman9@gmail.com>
2023-03-14 15:59:32 +0200

    replace the default data with a real data and create function to convert csv to array

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import ReactApexChart from 'react-apexcharts';
 
-import { defaultConfig } from '../../utils/chartDefaultConfig';
+import { defaultOptions } from '../../utils/chartDefaultConfig';
+
+import { CSVText, smallCSVText } from '../../mock/SPUS';
 
 import './style.css';
+import { convertCsvToArray } from '../../utils/convertCSVtoArray';
 
 const Chart = () => {
+  const data = convertCsvToArray(smallCSVText);
+
+  console.log(data);
+
+  const series = useMemo(
+    () => [
+      {
+        name: 'candle',
+        data,
+      },
+    ],
+    [data]
+  );
+
   return (
     <div className='chart'>
-      <ReactApexChart options={defaultConfig.options} series={defaultConfig.series} type='candlestick' height={350} />
+      <ReactApexChart options={defaultOptions} series={series} type='candlestick' height={350} />
     </div>
   );
 };
---
+++ src/utils/convertCSVtoArray.js
@@ -0,0 +1,8 @@
+export const convertCsvToArray = (data) =>
+  data
+    .split('\n') // split to lines
+    .slice(1) // remove the head line before split the line
+    .map(function (line) {
+      const array = line.split(','); // slit every line to array of items
+      return { x: array[0], y: array.slice(1, 5) }; // convert to the shape that the chart can read
+    });
