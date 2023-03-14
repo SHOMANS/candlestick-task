d6ca83ba27b8c0226e32835b1ceb24e40f769652
SHOMANS <mohammedshoman9@gmail.com>
2023-03-14 14:35:26 +0200

    create header and footer and use them as a simple layout

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../Container';
+
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>Copyright &copy; 2023</Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../Container';
+
+import './style.css';
+
+const Header = () => {
+  return (
+    <header>
+      <Container>Candlestick Chart</Container>
+    </header>
+  );
+};
+
+export default Header;
