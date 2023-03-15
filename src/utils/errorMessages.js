ba7fd45369333a16fb580752b54dc1aab2e29882
SHOMANS <mohammedshoman9@gmail.com>
2023-03-15 16:47:40 +0200

    handle error messages in yup and compare between dates

---
+++ src/utils/errorMessages.js
@@ -0,0 +1,9 @@
+export const requiredMessage = 'this field is required';
+
+export const atLeastOne = 'value should be at least 1';
+
+export const greaterThan = 'to date must be greater than from date';
+
+export const typeNumber = 'value should be a number';
+export const typeString = 'value should be a string';
+export const typeDate = 'value should be a date';
--- src/utils/schema.js
+++ src/utils/schema.js
@@ -1,10 +1,19 @@
 import * as yup from 'yup';
+import { atLeastOne, greaterThan, requiredMessage, typeDate, typeNumber, typeString } from './errorMessages';
 
-export const schema = yup
-  .object({
-    interval: yup.number().min(1).required(),
-    type: yup.string().required(),
-    from: yup.date(),
-    to: yup.date(),
-  })
-  .required();
+export const schema = yup.object({
+  interval: yup.number().typeError(typeNumber).min(1, atLeastOne).required(requiredMessage),
+  type: yup.string(typeString).required(requiredMessage),
+  from: yup.date().typeError(typeDate).required(requiredMessage),
+  to: yup
+    .date(typeDate)
+    .typeError(typeDate)
+    .when('from', (from, schema) => {
+      try {
+        return from && schema.min(from, greaterThan);
+      } catch (error) {
+        return schema;
+      }
+    })
+    .required(requiredMessage),
+});
